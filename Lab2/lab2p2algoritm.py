e036e9849c00179180213a8e33981f73b8422db4
Egor Chernykh <chernihegor3145@gmail.com>
2026-09-24 17:03:58 +0700

    Алгоритм функции поиска пар ближайших точек в 2-мерном пространстве+проверка юниттестов

--- Lab2/lab2p2algoritm.py
+++ Lab2/lab2p2algoritm.py
@@ -1,4 +1,22 @@
+import unittest
 from random import randint
+
+class test_lab(unittest.TestCase):
+
+    def test_closets_pair_and_distance(self):
+        self.assertEqual(unittest_result([])[1:], ([]))
+        self.assertEqual(unittest_result([[1,1]])[1:], ([]))
+        self.assertEqual(unittest_result([[2,3],[3,4]])[1:], ([2,3],[3,4]))
+        self.assertEqual(unittest_result([[2,3],[1,9],[6,2]])[1:], ([6,2],[2,3]))
+        self.assertEqual(unittest_result([[2,9],[2,4],[2,1],[2,-8]])[1:], ([2,1],[2,4]))
+        self.assertEqual(unittest_result([[2,3],[0,4],[11,9],[2,8],[4,4],[3,6],[6,5],[1,9]])[1:], ([2,8],[1,9]))
+        self.assertEqual(unittest_result([[5,6],[1,2],[4,-2],[-9,0],[-1,-2],[0,7],[2,-1],[-3,1]])[1:], ([4,-2],[2,-1]))
+        self.assertEqual(unittest_result([[-1,20],[-1.5,10],[-2,-10],[-2.7,-20],[-10,20],[-10.5,10],[-11.7,-10],[-12.2,-20],[1,21],[1.5,11],[2,-9],[2.7,-19],[10,21],[10.5,11],[11.7,-9],[12.2,-19]])[1:], ([-1,20],[1,21]))
+        self.assertEqual(unittest_result([[2,2],[2,3]])[0], 1)
+        self.assertEqual(unittest_result([[3,3],[-1,3]])[0], 4)
+        self.assertEqual(unittest_result([[2.7,1.5],[4.7,2.5]])[0], 5**0.5)
+
+
 
 class Point:
     def __init__(self, x, y, id):
@@ -8,6 +26,28 @@ class Point:
 
     def __repr__(self) -> str:
         return f'[{self.x}, {self.y}]'
+
+def unittest_points(_list):
+    points = []
+
+    for i in range(len(_list)):
+        points.append(Point(_list[i][0], _list[i][1], i))
+
+    return points
+
+def unittest_result(_list):
+    if len(_list) <= 1:
+        return _list
+    points = unittest_points(_list)
+    d, p1, p2 = calc_min_dist(points)
+    ans = []
+    for i in points:
+        if i.id == p1:
+            ans.append(i)
+        if i.id == p2:
+            ans.append(i)
+    
+    return dist(ans[0], ans[1]), [ans[0].x, ans[0].y], [ans[1].x, ans[1].y]
 
 def dist(a, b):
     return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5
@@ -60,7 +100,7 @@ def calc_min_dist(points):
             close_points = list(filter(lambda point: abs(point.x - middle_x) < min_d, points))
             
             for i in range(len(close_points)):
-                for j in range(i - 1, 0, -1):
+                for j in range(i - 1, -1, -1):
                     p1 = close_points[i]
                     p2 = close_points[j]
                     if p1.y - p2.y > min_d:
@@ -73,26 +113,27 @@ def calc_min_dist(points):
     return calc_rec(points, 0, len(points))
 
 if __name__ == '__main__':
-    points = gen_random_point(2)
-
-    print(*points)
-    d, p1, p2 = calc_min_dist(points)
-    d1, p11, p21 = calc_min_dist_slow(points)
-
-    ans = []
-    for i in points:
-        if i.id == p1:
-            ans.append(i)
-        if i.id == p2:
-            ans.append(i)
+    unittest.main()
+    # points = gen_random_point(2)
     
-    print(dist(ans[0], ans[1]), ans[0], ans[1])
+    # print(*points)
+    # d, p1, p2 = calc_min_dist(points)
+    # d1, p11, p21 = calc_min_dist_slow(points)
 
-    ans1 = []
-    for i in points:
-        if i.id == p11:
-            ans1.append(i)
-        if i.id == p21:
-            ans1.append(i)
+    # ans = []
+    # for i in points:
+    #     if i.id == p1:
+    #         ans.append(i)
+    #     if i.id == p2:
+    #         ans.append(i)
     
-    print(dist(ans[0], ans[1]), ans[0], ans[1])
+    # print(dist(ans[0], ans[1]), ans[0], ans[1])
+
+    # ans1 = []
+    # for i in points:
+    #     if i.id == p11:
+    #         ans1.append(i)
+    #     if i.id == p21:
+    #         ans1.append(i)
+    
+    # print(dist(ans[0], ans[1]), ans[0], ans[1])
